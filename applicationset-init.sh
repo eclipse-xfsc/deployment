714aac560fcb070cc181e72be20fb237bb9da990
Steffen Schulze <67590336+SchulzeStTSI@users.noreply.github.com>
2026-07-20 00:38:24 +0200

    update in helms

--- applicationset-init.sh
+++ applicationset-init.sh
@@ -1,2 +1,5 @@
 helm install -n security infra-namespace INFRA/app-management/app-namespace -f INFRA/app-management/app-namespace-values/infra-values.yaml
-helm install -n infrastructure storage XFSC/Applicationsets/chart -f XFSC/Applicationsets/values/storage-values.yaml
+
+STORAGE_CLASS=$(kubectl get storageclass -o jsonpath='{.items[?(@.metadata.annotations.storageclass\.kubernetes\.io/is-default-class=="true")].metadata.name}')
+
+helm install -n infrastructure storage XFSC/Applicationsets/chart -f XFSC/Applicationsets/values/storage-values.yaml --set storageClass="$STORAGE_CLASS"
