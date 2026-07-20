4be5c68d4ed7893318d9971ccdff87715a4ab63b
Steffen Schulze <67590336+SchulzeStTSI@users.noreply.github.com>
2026-07-20 18:20:09 +0200

    fix in scripts and worker network

--- basic-cluster-init.sh
+++ basic-cluster-init.sh
@@ -1,9 +1,20 @@
 kubectl create namespace observabilty
+helm repo add otel https://open-telemetry.github.io/opentelemetry-helm-charts
+helm repo add prom https://prometheus-community.github.io/helm-charts
+helm repo add grafana https://grafana.github.io/helm-charts
+helm repo add jaeger https://jaegertracing.github.io/helm-charts
+helm dependency build INFRA/observability/opentelemtry
 helm install -n observability observability INFRA/observability/opentelemtry
 kubectl create namespace security
+helm repo add openbao https://openbao.github.io/openbao-helm
+helm dependency build INFRA/security/openbao
 helm install -n security openbao INFRA/security/openbao
+helm repo add eso https://charts.external-secrets.io
+helm dependency build INFRA/security/eso
 helm install -n security eso INFRA/security/eso
+helm repo add kyverno https://kyverno.github.io/kyverno/
+helm dependency build INFRA/security/kyverno
 helm install -n security kyverno INFRA/security/kyverno
+./argocd-bootstrap.sh
 helm dependency build INFRA/app-management/kubernetes-operator
 helm install -n default kubernetes-operator INFRA/app-management/kubernetes-operator
-./argocd-bootstrap.sh
