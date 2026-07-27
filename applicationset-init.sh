22b418ee62afa5047ae579b62ecf91d2058672f8
Steffen Schulze <67590336+SchulzeStTSI@users.noreply.github.com>
2026-07-27 23:59:25 +0200

    first ocm w-stack modifcations

--- applicationset-init.sh
+++ applicationset-init.sh
@@ -27,3 +27,5 @@ kubectl create secret generic external-dns \
 helm install -n infrastructure storage XFSC/Applicationsets/chart -f XFSC/Applicationsets/values/03_storage-values.yaml --set storageClass="$STORAGE_CLASS"
 helm install -n infrastructure network XFSC/Applicationsets/chart -f XFSC/Applicationsets/values/04_network-values.yaml --set storageClass="$STORAGE_CLASS" --set profile="$PROFILE" --set domain="$DOMAIN" --set email="$EMAIL" 
 helm install -n infrastructure core XFSC/Applicationsets/chart -f XFSC/Applicationsets/values/05_core-values.yaml --set storageClass="$STORAGE_CLASS"
+helm install -n infrastructure core XFSC/Applicationsets/chart -f XFSC/Applicationsets/values/06_ocm_wstack-values.yaml --set storageClass="$STORAGE_CLASS"
+
